0fde0a89b6599f44b0ba1b80cb07178d5808381c
KocoTiger <98165704+KocoTiger@users.noreply.github.com>
2022-01-24 00:19:06 +0900

    생활코딩_Javascript 마지막

---
+++ test.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <style>
+
+
+    </style>
+
+  </head>
+  <body>
+
+  ~2
+  <script type="text/javascript">
+  alert
+
+
+
+  var 당첨자수 = 2;
+  var 댓글선택자 = '._3b-9>div>.UFIComment .UFICommentActorName';
+  function shuffle(a) {
+      for (let i = a.length; i; i--) {
+          let j = Math.floor(Math.random() * i);
+          [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+  }
+  var list = [];
+  document.querySelectorAll(댓글선택자).forEach(function(e){
+      list.push(e.innerText);
+  });
+  list = list.filter((v,i,a) => a.indexOf(v)===i);
+  shuffle(list)
+  console.log(list.slice(0,당첨자수));
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
